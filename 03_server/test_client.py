12183690f0e2a4ef89b2e978df35654285a4213f
snapthecoder <Dominic@Dominics-MacBook-Pro.local>
2018-05-27 16:48:16 +0200

    only log no cdv, don't close conn when recv

--- 03_server/test_client.py
+++ 03_server/test_client.py
@@ -1,6 +1,19 @@
+import socket
 import time
 
-ts = time.time()
+host = "192.168.0.101"
+port = 8000
 
-print(ts)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2.connect((host, port))
+
+while True:
+    s.sendall('STX1,000000000000000000002000,-25ETX')
+    s2.sendall('STX2,000000000000000000001000,-26ETX')
+    time.sleep(1)
+
+
 
